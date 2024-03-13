7b462569efc1d5878f754f002980ea2ba9d92452
pakfron <pakpoom.poom@hotmail.com>
2024-03-13 23:37:40 +0700

    interceptor define respone

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,25 +1,47 @@
 import {
-    UseInterceptors,
-    NestInterceptor,
-    ExecutionContext,
-    CallHandler
-} from '@nestjs/common'
-import { Observable} from 'rxjs'
-import {map}from 'rxjs/operators'
-import { plainToClass} from 'class-transformer'
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToClass } from 'class-transformer';
+
+interface ClassConstructor {
+    new (...args:any[]):{};
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
 
 export class SerializeInterceptor implements NestInterceptor {
-intercept(context:ExecutionContext,next:CallHandler) :Observable<any> {
-// Run something before a request is handled
-// by the request handler
-console.log("I'm running before the handler",context)
-
-return next.handle().pipe(
-    map((data:any)=>{
-        // Run something before the response is sent out
-        console.log(`I'm running before response is sent out`, data)
-    })
-)
+  constructor(private dto: any) {}
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> {
+    return next.handle().pipe(
+      map((data: any) => {
+        return plainToClass(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
 }
 
-}
+//example
+// intercept(context:ExecutionContext,next:CallHandler) :Observable<any> {
+//     // Run something before a request is handled
+//     // by the request handler
+//     console.log("I'm running before the handler",context)
+
+//     return next.handle().pipe(
+//         map((data:any)=>{
+//             // Run something before the response is sent out
+//             console.log(`I'm running before response is sent out`, data)
+//         })
+//     )
+//     }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,8 +14,11 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
-import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
+
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
   
@@ -23,10 +26,11 @@ export class UsersController {
   createUser(@Body() body: CreateUserDto) {
     this.usersService.create(body.email, body.password);
   }
-  @UseInterceptors(SerializeInterceptor)
+  // @UseInterceptors(Serialize(UserDto))
+
   @Get('/:id')
   async findUser(@Param('id') id: string) {
-    console.log(`handler is running`)
+    
     const user = await this.usersService.findOne(parseInt(id));
     if (!user) {
       throw new NotFoundException('user not found');
