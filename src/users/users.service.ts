df273fa40c1bbc62ce4ccde184cfbc84da604beb
pakfron <pakpoom.poom@hotmail.com>
2024-03-13 16:31:57 +0700

    find and update user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,9 +6,35 @@ import { User } from './user.entity';
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
-   
-  create(email:string,password:string) {
-    const user = this.repo.create({email,password})
+
+  create(email: string, password: string) {
+
+
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+
+  findOne(id:number) {
+  // findOne with findOne
+  // this.repo.findOne({where:{id}})
+           //find user where id raw query
+    return this.repo.query(`SELECT * FROM "User" where id = ${id}`);
+  }
+  find(email:string)  {
+    //find 
+    // this.repo.find({where:{email}})
+    return this.repo.query(`SELECT * FROM "User" where email = ${email}`)
+  }
+
+ async update(id:number,attr: Partial<User>) {
+    const user =  await this.repo.findOne({where:{id}})
+    if (!user) {
+      throw new Error('user not found')
+    }
+    Object.assign(user,attr)
+
     return this.repo.save(user)
-}
+
+  }
+  remove() {}
 }
